23155f1429666a189053962bb34e8b06dc88f1da
jiewei.ljw <jiewei.ljw@alibaba-inc.com>
2020-02-29 22:14:54 +0800

    s/remote_/current_/g

--- brew-pr.sh
+++ brew-pr.sh
@@ -5,7 +5,7 @@ version="latest"
 npm_registry="https://registry.npmjs.org"
 # npm_registry="https://registry.npm.taobao.org"
 
-remote_formula_url="https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/whistle.rb"
+current_formula_url="https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/whistle.rb"
 latest_tarball=`curl -s $npm_registry/whistle/$version | grep -E -o '"tarball":"[^"]+"'| cut -d'"' -f 4`
 latest_sha256=""
 
@@ -13,14 +13,14 @@ tmp_dir=`mktemp -d`
 filename="$tmp_dir/`basename $latest_tarball`"
 formula_filename="$tmp_dir/whistle.rb"
 
-curl -s $remote_formula_url -o $formula_filename
-remote_sha256=`grep -E -m 1 'sha256 ".+"' $formula_filename | cut -d'"' -f 2`
-remote_tarball=`grep -E -m 1 'url "https:[^"]+"' $formula_filename | cut -d'"' -f 2`
+curl -s $current_formula_url -o $formula_filename
+current_sha256=`grep -E -m 1 'sha256 ".+"' $formula_filename | cut -d'"' -f 2`
+current_tarball=`grep -E -m 1 'url "https:[^"]+"' $formula_filename | cut -d'"' -f 2`
 
-remote_tarball_basename=`basename $remote_tarball`
+current_tarball_basename=`basename $current_tarball`
 latest_tarball_basename=`basename $latest_tarball`
 
-if [[ $remote_tarball_basename == $latest_tarball_basename ]]; then
+if [[ $current_tarball_basename == $latest_tarball_basename ]]; then
   echo '👋 no need to pr'
   exit 0
 fi
@@ -33,9 +33,9 @@ else
   latest_sha256=`sha256sum $filename | cut -d ' ' -f 1`
 fi
 
-echo "remote_tarball=$remote_tarball"
+echo "current_tarball=$current_tarball"
 echo "latest_tarball=$latest_tarball"
-echo "remote_sha256=$remote_sha256"
+echo "current_sha256=$current_sha256"
 echo "latest_sha256=$latest_sha256"
 
 pr_cmd="brew bump-formula-pr --strict whistle --url=${latest_tarball} --sha256=${latest_sha256}"
